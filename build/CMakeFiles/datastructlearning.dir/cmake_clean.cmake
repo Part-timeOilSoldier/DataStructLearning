4f357f4b0f8900d6650dc9394fb982f16fb93b6f
Part-timeOilSoldier <shuaibianyang666@gmail.com>
2025-12-03 18:34:53 +0800

    Initial project commit, including CMake, Seething

---
+++ build/CMakeFiles/datastructlearning.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastructlearning.dir/src/lib/ui.cpp.obj"
+  "CMakeFiles/datastructlearning.dir/src/lib/ui.cpp.obj.d"
+  "CMakeFiles/datastructlearning.dir/src/main.cpp.obj"
+  "CMakeFiles/datastructlearning.dir/src/main.cpp.obj.d"
+  "D:/MyCodes/CPP/DataStructLearning/bin/datastructlearning.exe"
+  "D:/MyCodes/CPP/DataStructLearning/bin/datastructlearning.exe.manifest"
+  "D:/MyCodes/CPP/DataStructLearning/bin/datastructlearning.pdb"
+  "libdatastructlearning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastructlearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/datastructlearning.dir/src/lib/ui.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/datastructlearning.dir/src/lib/ui.cpp.obj: \
+ D:\MyCodes\CPP\DataStructLearning\src\lib\ui.cpp
